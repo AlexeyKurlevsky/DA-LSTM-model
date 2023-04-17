710876720248256f90ccfb216169e3fcb090950a
AlexeyKurlevsky <kurlevsky@gmail.com>
2023-04-17 21:34:20 +0300

    Add window generators

--- src/window_generators/window_generator_interval.py
+++ src/window_generators/window_generator_interval.py
@@ -12,18 +12,14 @@ from typing import Any, Tuple, List
 from sklearn.preprocessing import StandardScaler
 
 
-def create_time_steps(length):
-    return list(range(-length, 0))
-
-
-class WindowGenerator:
+class WindowGeneratorOneTarget:
     def __init__(self, data: pd.DataFrame, conf: Any, mean_flg: bool = False, scaler: Any = StandardScaler()):
         self.data = data
         self.conf = conf
         self.mean_flg = mean_flg
         self.scaler = scaler
 
-    def get_standart_data(self) -> pd.DataFrame:
+    def get_standard_data(self) -> pd.DataFrame:
         data_train = self.data.iloc[:self.conf.train_split, :]
         data_test = self.data.iloc[self.conf.train_split:, :]
         col_name = self.data.columns
@@ -34,11 +30,11 @@ class WindowGenerator:
             df_mean.dropna(inplace=True)
             df_scaled = pd.concat([df_mean, df_test_scaled])
         self.conf.n_samples = df_scaled.shape[0]
-        assert self.conf.val_split + self.conf.n_future <= self.conf.n_samples, f'Некорректное разбиение'
+        assert self.conf.val_split + self.conf.n_future <= self.conf.n_samples, f'Некорректное разбиение. Отсутствует тестовая выборка'
         return df_scaled
 
-    def plot_standart_data(self) -> None:
-        df_scaled = self.get_standart_data()
+    def plot_standard_data(self) -> None:
+        df_scaled = self.get_standard_data()
         ax = df_scaled.plot(subplots=True)
         for ax_i in ax:
             ax_i.axvline(df_scaled.index[self.conf.train_split], color='k', linestyle='--')
@@ -58,7 +54,8 @@ class WindowGenerator:
         return [np.array(X), np.array(y)]
 
     def get_data_to_model(self) -> List[np.ndarray]:
-        df_scaled = self.get_standart_data()
+        df_scaled = self.get_standard_data()
+
         X_train, y_train = self._split_series(df_scaled.iloc[:self.conf.train_split, :].values)
         X_val, y_val = self._split_series(
             df_scaled.iloc[self.conf.train_split - self.conf.window_size:self.conf.val_split, :].values)
@@ -91,13 +88,13 @@ class WindowGenerator:
 
     def plot_validation_window(self, model: Any) -> None:
         X_train, y_train, X_val, y_val, X_test, y_test = self.get_data_to_model()
-        fig, ax = plt.subplots(2, 2, figsize=(20, 15))
+        fig, ax = plt.subplots(2, 2, figsize=(17, 15))
         ax = ax.ravel()
         window_list = [0, 20, 40, 60]
-        df_scaled = self.get_standart_data()
+        df_scaled = self.get_standard_data()
         df_val = df_scaled.iloc[self.conf.train_split - self.conf.window_size:self.conf.val_split, :]
         date_val = df_val.index.values
-        pred_val = model.predict(X_val)
+        pred_val = model.predict_interval(X_val, self.conf.n_future).numpy()
         for ind, val in enumerate(window_list):
             assert X_val.shape[0] - 1 >= val, f"Окна c номером {val} не существует"
             y_val_inv, pred_val_inv = self.get_inverse_values(y_val[val, :], pred_val[val, :])
@@ -116,16 +113,17 @@ class WindowGenerator:
             ax[ind].tick_params(axis='x', labelrotation=45)
             ax[ind].grid()
             ax[ind].legend()
+            IPython.display.clear_output()
 
     def plot_test_window(self, model: Any) -> None:
         X_train, y_train, X_val, y_val, X_test, y_test = self.get_data_to_model()
-        fig, ax = plt.subplots(1, 2, figsize=(20, 10))
+        fig, ax = plt.subplots(1, 2, figsize=(12, 8))
         ax = ax.ravel()
         window_list = [0, 20]
-        df_scaled = self.get_standart_data()
+        df_scaled = self.get_standard_data()
         df_test = df_scaled.iloc[self.conf.val_split - self.conf.window_size:, :]
         date_test = df_test.index.values
-        pred_test = model.predict(X_test)
+        pred_test = model.predict_interval(X_test, self.conf.n_future).numpy()
         for ind, val in enumerate(window_list):
             assert X_test.shape[0] - 1 >= val, f"Окна с номером {val} не существует"
             y_test_inv, pred_test_inv = self.get_inverse_values(y_test[val, :], pred_test[val, :])
@@ -144,10 +142,11 @@ class WindowGenerator:
             ax[ind].tick_params(axis='x', labelrotation=45)
             ax[ind].grid()
             ax[ind].legend()
+            IPython.display.clear_output()
 
     def calc_validation_mape(self, model: Any) -> List[float]:
         X_train, y_train, X_val, y_val, X_test, y_test = self.get_data_to_model()
-        pred_val = model.predict(X_val)
+        pred_val = model.predict_interval(X_val, self.conf.n_future).numpy()
         list_num_wind = np.arange(X_val.shape[0])
         list_metric = []
         for wind in list_num_wind:
@@ -168,7 +167,7 @@ class WindowGenerator:
 
     def calc_test_mape(self, model: Any) -> List[float]:
         X_train, y_train, X_val, y_val, X_test, y_test = self.get_data_to_model()
-        pred_test = model.predict(X_val)
+        pred_test = model.predict_interval(X_test, self.conf.n_future).numpy()
         list_num_wind = np.arange(X_test.shape[0])
         list_metric = []
         for wind in list_num_wind:
@@ -187,8 +186,8 @@ class WindowGenerator:
         arr = np.array(list_metric)
         return np.median(arr)
 
-    def plot_chart_mape_window(self, model: Any) -> None:
-        fig, ax = plt.subplots(1, 2, figsize=(18, 10))
+    def plot_chart_mape_window(self, model: Any) -> float:
+        fig, ax = plt.subplots(1, 2, figsize=(12, 8))
         ax = ax.ravel()
         val_mape = self.calc_validation_mape(model)
         test_mape = self.calc_test_mape(model)
@@ -206,3 +205,4 @@ class WindowGenerator:
         ax[1].set_ylabel('MAPE')
         ax[1].grid()
         plt.show()
+        IPython.display.clear_output()
