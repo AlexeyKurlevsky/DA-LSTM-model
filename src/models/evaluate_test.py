93e0fcb1b2c4b27ca585ab1f0d9944fdaa43a7d6
AlexeyKurlevsky <kurlevsky@gmail.com>
2023-07-05 23:25:06 +0300

    Add train model in dvc

--- src/models/evaluate_test.py
+++ src/models/evaluate_test.py
@@ -10,34 +10,27 @@ import yaml
 from sklearn.preprocessing import MinMaxScaler
 
 from models import DualAttentionRNN
-from src import (
-    Config,
-    seed_everything,
-    WindowGenerator,
-    calc_test_metric,
-    plot_test_window,
-)
-from src.visualization.calculate_metrics import calc_validation_metric
-from src.visualization.plot_window import plot_validation_window
+from src import Config, seed_everything, WindowGenerator
+from src.visualization.calculate_metrics import calc_test_metric
 
 
 @click.command()
 @click.argument("input_path", type=click.Path())
 @click.argument("model_feature_path", type=click.Path())
 @click.argument("output_metric_average", type=click.Path())
-@click.argument("output_validation_metric_all", type=click.Path())
-def evaluate_validation(
+@click.argument("output_test_metric_all", type=click.Path())
+def evaluate_test(
     input_path: str,
     model_feature_path: str,
     output_metric_average: str,
-    output_validation_metric_all: str,
+    output_test_metric_all: str,
 ):
     """
     Function for predict values.
     :param input_path: path processed data
     :param model_feature_path: directory with model
+    :param output_test_metric_all: path to save metric on all windows (CSV)
     :param output_metric_average: path to save average metric (JSON)
-    :param output_validation_metric_all: path to save metric on all windows (CSV)
     """
     logging.basicConfig(level=logging.INFO)
     params = yaml.safe_load(open("params.yaml"))["train"]
@@ -68,12 +61,12 @@ def evaluate_validation(
 
     X_train, y_train, X_val, y_val, X_test, y_test = w_one_target.get_data_to_model()
     logging.info("Predict interval")
-    y_pred = da_model.predict_interval(X_val, w_one_target.conf.n_future)
-    mape_arr, rmse_arr = calc_validation_metric(w_one_target, y_pred)
+    y_pred = da_model.predict_interval(X_test, w_one_target.conf.n_future)
+    mape_arr, rmse_arr = calc_test_metric(w_one_target, y_pred)
 
     logging.info("Calculate all windows metric")
     df_metric_all_window = pd.DataFrame(data={"MAPE": mape_arr, "RMSE": rmse_arr})
-    df_metric_all_window.to_csv(output_validation_metric_all, index=False)
+    df_metric_all_window.to_csv(output_test_metric_all, index=False)
 
     logging.info("Calculate average window metric")
     df_average_metric = {"MAPE": np.average(mape_arr), "RMSE": np.average(rmse_arr)}
@@ -84,4 +77,4 @@ def evaluate_validation(
 
 
 if __name__ == "__main__":
-    evaluate_validation()
+    evaluate_test()
